74927f26d8b8410016e78065fb6843513e365b83
krystlkayem <krystalkayem@gmail.com>
2025-05-24 12:24:49 +0800

    Database

---
+++ CampingDeckDL/DBDataService.cs
@@ -0,0 +1,52 @@
+﻿using RentalCommon;
+using Microsoft.Data.Sql;
+using Microsoft.Data.SqlClient;
+
+namespace CampingDeckDL
+{
+    public class DBDataService : IRentalDataService
+    {
+
+        static string connectionString
+    = "Data Source =; Initial Catalog = CampingRental; Integrated Security = True; TrustServerCertificate=True;";
+
+        static SqlConnection sqlConnection;
+
+        public DBDataService()
+        {
+            sqlConnection = new SqlConnection(connectionString);
+        }
+
+        public List<CampingCommon> GetItems()
+        {
+            string selectStatement = "SELECT Name, Item, Quantity";
+            sqlCommand selectCommand = new sqlCommand(selectStatement), sqlConnection;
+
+            sqlConnection.Open(); 
+
+
+            //jijij
+
+            SqlDataReader reader = selectCommand.ExecuteReader();
+            List<CampingCommon> items = new List<CampingCommon>();
+            while (reader.Read())
+            {
+                Name = reader["Name"].ToString();
+                Item = reader["Item"].ToString();
+                Quantity = Convert.ToInt32(reader["Quantity"]);
+            };
+            items.Add(item);
+        }
+
+
+        public void UpdateItem(CampingCommon item)
+        {
+            sqlConnection.Open();
+
+            var updateStatement = $"";
+
+            sqlConnection.ExecuteNonQuery();
+            sqlConnection.Close();
+        }
+    }
+}
